3fc156c66674326e6c4d4c4ae1a16a191cd55a98
jingningdeng <1615103105@qq.com>
2021-01-15 18:22:52 +0800

    akn

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module renovate-demo1
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.0
+require (
+	github.com/dlclark/regexp2 v1.1.0
+	github.com/gin-gonic/gin v1.6.0
+)
--- main/main.go
+++ main/main.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/dlclark/regexp2"
 )
 
 func main() {
 	// 初始化空的服务器
 	app := gin.New()
-
+	_ = regexp2.Debug
 	log.Printf("启动服务器在 http address: %s", ":8080")
 	log.Printf(http.ListenAndServe(":8080", app).Error())
 }
